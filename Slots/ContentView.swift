304fdd5b26f4efc4e737d1a0e8dbb2ecff093594
Umang Kumar <umangkumar1324@gmail.com>
2020-08-26 11:39:22 -0700

    initial commit

---
+++ Slots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots
+//
+//  Created by Umang Kumar on 8/26/20.
+//  Copyright © 2020 Umang Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
