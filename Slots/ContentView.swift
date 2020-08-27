f61af22b981559da608f544a2237daf327e148b7
Umang Kumar <umangkumar1324@gmail.com>
2020-08-27 15:18:53 -0700

    first iteration of app

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -9,8 +9,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var symbols = ["apple","star","cherry"]
+    @State private var nums = [0,0,0]
+    @State private var credits = 1000
+    private var betAmount = 5
+    private var winAmount = 10
+
+    
+    //BackGround
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees:45))
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                //Title
+                HStack{
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Text("SwiftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2)
+                Spacer()
+                //Credit Counter
+                
+                Text("Credits: " + String(credits) )
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                Spacer()
+                //Cards
+                
+                HStack{
+                    Spacer()
+                    
+                    Image(symbols[nums[0]])
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+
+                    Image(symbols[nums[1]])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    Image(symbols[nums[2]])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    //change the image
+                    self.nums[0] = Int.random(in:
+                        0...self.symbols.count-1)
+                    self.nums[1] = Int.random(in:
+                    0...self.symbols.count-1)
+                    self.nums[2] = Int.random(in:
+                    0...self.symbols.count-1)
+                    
+                    //check if all the picture are the same
+                    
+                    if self.nums[0] == self.nums[1] && self.nums[1] == self.nums[2]{
+                        self.credits += self.winAmount
+                    }
+                    else{
+                        self.credits -= self.betAmount
+                    }
+                }){
+                    Text("SPIN")
+                    .bold()
+                    .padding(7)
+                    .padding([.leading,.trailing], 30)
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(20)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
